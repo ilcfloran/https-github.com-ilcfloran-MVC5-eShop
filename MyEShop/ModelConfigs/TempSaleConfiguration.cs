33bb8a647abb25ea29a1ac687d5592f35e9b35a8
ilcfloran <komail.shahmir@gmail.com>
2017-03-28 13:27:15 +0430

    Add TempSale

---
+++ MyEShop/ModelConfigs/TempSaleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MyEShop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace MyEShop.ModelConfigs
+{
+    public class TempSaleConfiguration : EntityTypeConfiguration<TempSale>
+    {
+
+        public TempSaleConfiguration()
+        {
+
+            Property(t => t.ShoppingCartId).IsRequired().HasColumnAnnotation("Index",
+                new IndexAnnotation(new IndexAttribute("IX_TempSale_ShoppingCartId") { IsUnique = false }));
+        }
+    }
+}
---
+++ MyEShop/Models/TempSale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEShop.Models
+{
+    public class TempSale
+    {
+        public int Id { get; set; }
+
+        public int ShoppingCartId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string BankGetNo { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
