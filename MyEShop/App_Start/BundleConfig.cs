126d92064b4330d601df92580a7e601b7c14cf29
ilcfloran <komail.shahmir@gmail.com>
2017-03-29 19:01:09 +0430

    Adde New Theme

--- MyEShop/App_Start/BundleConfig.cs
+++ MyEShop/App_Start/BundleConfig.cs
@@ -21,11 +21,21 @@ namespace MyEShop
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/jquery.scrollUp.min.js",
+                      "~/Scripts/price-range.js",
+                      "~/Scripts/jquery.prettyPhoto.js",
+                       "~/Scripts/main.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/prettyPhoto.css",
+                      "~/Content/price-range.css",
+                      "~/Content/animate.css",
+                      "~/Content/site.css",
+                      "~/Content/responsive.css"));
         }
     }
 }
