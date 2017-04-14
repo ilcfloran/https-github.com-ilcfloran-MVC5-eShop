bd545d785eab0058d12d55bf618de4bdcfe8c750
ilcfloran <komail.shahmir@gmail.com>
2017-04-14 21:57:06 +0430

    User can bid and see the result

--- MyEShop/Controllers/AuctionsController.cs
+++ MyEShop/Controllers/AuctionsController.cs
@@ -1,4 +1,5 @@
-﻿using MyEShop.Core.Models;
+﻿using Microsoft.AspNet.Identity;
+using MyEShop.Core.Models;
 using MyEShop.DataAccess.ModelConfigs;
 using System.Data.Entity;
 using System.Linq;
@@ -32,22 +33,64 @@ namespace MyEShop.Controllers
             var user = db.Products.Include("User").Where(p => p.Id == id)
                 .Select(p => new
                 {
+                    Id = p.UserId,
                     FullName = p.User.FirstName + " " + p.User.LastName,
                     MemberSince = p.User.MemberSince,
                     City = p.User.City
                 })
                 .SingleOrDefault();
 
+
+            //var userBid = db.Auctions.Where(a => a.UserId == user.Id).SingleOrDefault();
+
+            var biderId = User.Identity.GetUserId();
+            var biderOffer = db.Auctions.Where(a => a.UserId == biderId && a.ProductId == id).Select(a => a.Price).SingleOrDefault();
+
+            ViewBag.UserBid = biderOffer;
             ViewBag.UserName = user.FullName;
             ViewBag.MemberSince = user.MemberSince;
             ViewBag.City = user.City;
             ViewBag.AuctionCount = auctions.Count();
+            ViewBag.ProductId = id;
+
+
 
             return PartialView("_AuctionByProduct", auctions.OrderByDescending(a => a.Price).Take(3));
         }
 
 
+        public ActionResult Offer(int bid, int pId)
+        {
+            // check if user previously bid for the same product, if did, update the data
+            //check if the person who added the product for auction cannot bid
 
+            if (bid > 0)
+            {
+                var biderId = User.Identity.GetUserId();
+                var userBid = db.Auctions.Where(a => a.UserId == biderId && a.ProductId == pId).SingleOrDefault();
+                if (userBid != null)
+                {
+                    userBid.Price = bid;
+                    db.Entry(userBid).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    var newAuction = new Auction()
+                    {
+                        Price = bid,
+                        ProductId = pId,
+                        User = db.Users.Where(u => u.Id == biderId).SingleOrDefault(),
+                        UserId = biderId,
+                        Win = false
+                    };
+                    db.Auctions.Add(newAuction);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("AuctionByProduct", new { id = pId });
+        }
 
 
 
