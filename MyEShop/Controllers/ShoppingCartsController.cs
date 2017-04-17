5c1d271ead8b5d0f590fee827c17eaf99f08b19c
ilcfloran <komail.shahmir@gmail.com>
2017-04-17 14:10:47 +0430

    Added ShoppingCart Controller

---
+++ MyEShop/Controllers/ShoppingCartsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyEShop.Core.Models;
+using MyEShop.DataAccess.ModelConfigs;
+
+namespace MyEShop.Controllers
+{
+    public class ShoppingCartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ShoppingCarts
+        public ActionResult Index()
+        {
+            return View(db.ShoppingCart.ToList());
+        }
+
+        // GET: ShoppingCarts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCart.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShoppingCarts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ProductId,ProductName,UserId,Date,Status,Count,Price")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShoppingCart.Add(shoppingCart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCart.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ProductId,ProductName,UserId,Date,Status,Count,Price")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shoppingCart).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCart.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ShoppingCart shoppingCart = db.ShoppingCart.Find(id);
+            db.ShoppingCart.Remove(shoppingCart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
